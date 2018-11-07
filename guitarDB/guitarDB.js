8f8c3dd39bc907cca25159ad6b600476476f6acf
Zack Shorts <shorts23@gmail.com>
2018-11-07 16:41:42 -0700

    made the scraper output its results in the output folder as JSON

--- guitarDB/guitarDB.js
+++ guitarDB/guitarDB.js
@@ -1,5 +1,10 @@
 const cheerio = require('cheerio');
 const request = require('request');
+const fs = require('fs');
+
+var object = {
+    guitarists: []
+};
 
 request({
     method: 'GET',
@@ -10,10 +15,22 @@ request({
 
     let $ = cheerio.load(body);
 
-    let guitarists_name = $('div h2').text();
+    let guitarists_name = $('h2').text();
     let guitarists = guitarists_name.split('\n');
 
     guitarists.forEach((guitarist) => {
-        console.log(guitarist);
+        if(guitarist.replace(/^[ \t]+/g, '') !== '') object.guitarists.push(guitarist.replace(/^[ \t]+/g, ''));
+    });
+
+    let json = JSON.stringify(object);
+    fs.writeFile('output/guitarists_name_output.json', json, 'utf8', function(err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
     });
 });
+
+
+
