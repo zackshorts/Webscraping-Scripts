75593669b959d4ed657728cdf152c1225852cdcb
Zack Shorts <shorts23@gmail.com>
2018-11-07 18:38:13 -0700

    added image path

--- guitarDB/guitarDB.js
+++ guitarDB/guitarDB.js
@@ -1,9 +1,11 @@
 const cheerio = require('cheerio');
 const request = require('request');
 const fs = require('fs');
+const url = require('url');
 
-var object = {
-    guitarists: []
+let guitarist_object = {
+    guitarists_names: [],
+    img_paths: [],
 };
 
 request({
@@ -18,16 +20,20 @@ request({
     let guitarists_name = $('h2').text();
     let guitarists = guitarists_name.split('\n');
 
+    $(".span2 img").each(function(i, image) {
+        guitarist_object.img_paths.push(url.resolve('https://equipboard.com/role/guitarists', $(image).attr('data-original')));
+    });
+    console.log(guitarist_object.img_paths);
+
     guitarists.forEach((guitarist) => {
-        if(guitarist.replace(/^[ \t]+/g, '') !== '') object.guitarists.push(guitarist.replace(/^[ \t]+/g, ''));
+        if(guitarist.replace(/^[ \t]+/g, '') !== '') guitarist_object.guitarists_names.push(guitarist.replace(/^[ \t]+/g, ''));
     });
 
-    let json = JSON.stringify(object);
-    fs.writeFile('output/guitarists_name_output.json', json, 'utf8', function(err) {
+    let json = JSON.stringify(guitarist_object);
+    fs.writeFile('output/guitarists/guitarists_name_output.json', json, 'utf8', function(err) {
         if (err) {
             return console.log(err);
         }
-
         console.log("The file was saved!");
     });
 });
