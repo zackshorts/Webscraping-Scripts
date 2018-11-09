fd1b59ecb1fb54a90ccf89c2102f3e76054c0cb7
Zack Shorts <shorts23@gmail.com>
2018-11-08 20:20:22 -0700

    new JS file to find specific info for each guitarist

---
+++ guitarDB/guitarists/all_guitarists.js
@@ -0,0 +1,117 @@
+const cheerio = require('cheerio');
+const request = require('request');
+const fs = require('fs');
+const url = require('url');
+const axios = require('axios');
+
+// The main object that will be jsonified and imported into database
+let guitarist_object = [];
+
+// Used to request the specific URL for each guitarist
+let formatted_names = [];
+let finished_names = [];
+let finished_images = [];
+
+// This is where we scrape the names and image paths of all guitarists
+ getData = async () => {
+    axios.get('https://equipboard.com/role/guitarists')
+        .then((response) => {
+            if (response.status === 200) {
+                const html = response.data;
+                const $ = cheerio.load(html);
+
+                // This will select all of the names of the guitarists
+                let guitarists_name = $('h2').text();
+                let guitarists = guitarists_name.split('\n');
+
+                // This will replace any empty part of the array
+                // It will also format it to remove any leading white space
+                guitarists.map((guitarist) => {
+                    if (guitarist.replace(/^[ \t]+/g, '') !== '') return finished_names.push(guitarist.replace(/^[ \t]+/g, ''));
+                });
+
+                // This is used to get the specific info for each guitarist.
+                // Their info page uses a lower case and dashed pattern in the URL
+                formatted_names = finished_names.map((name) => {
+                    return (name.toLowerCase().replace(/\s|'/g, '-').replace(/\./g, ''));
+                });
+
+                // This will get all of the image urls for the guitarists
+                $(".span2 img").map(function (i, image) {
+                    finished_images.push(url.resolve('https://equipboard.com/role/guitarists', $(image).attr('data-original')));
+                });
+            }
+
+
+            // This for-loop will match up every guitarists with their image path
+            for(let i = 0; i < finished_names.length; i++) {
+                guitarist_object.push({
+                    guitarist_name: finished_names[i],
+                    img_path: finished_images[i]
+                })
+            }
+
+            // Write the object to a json object and output it to the correct folder
+            let json = JSON.stringify(guitarist_object);
+            fs.writeFile('guitarists/output/guitarists_name_output.json', json, 'utf8', function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("The file was saved!");
+            });
+
+            let json1 = JSON.stringify(formatted_names);
+            fs.writeFile('guitarists/output/guitarist_name_url.json', json1, 'utf8', function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("The url file was saved!");
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        })
+};
+
+
+getData();
+
+//     request('https://equipboard.com/role/guitarists', (err, res, body) => {
+//
+//         if (err) return console.error(err);
+//
+//         // body refers to the body of html content that we got from the requested URL
+//         let $ = cheerio.load(body);
+//
+//         // This will select all of the names of the guitarists
+//         let guitarists_name = $('h2').text();
+//         let guitarists = guitarists_name.split('\n');
+//
+//         // This will replace any empty part of the array
+//         // It will also format it to remove any leading white space
+//         guitarists.map((guitarist) => {
+//             if(guitarist.replace(/^[ \t]+/g, '') !== '') return finished_names.push(guitarist.replace(/^[ \t]+/g, ''));
+//         });
+//
+//         // This is used to get the specific info for each guitarist.
+//         // Their info page uses a lower case and dashed pattern in the URL
+//         formatted_names = finished_names.map((name) => {
+//             return (name.toLowerCase().replace(/\s|'/g, '-').replace(/\./g, ''));
+//         });
+//
+//         // This will get all of the image urls for the guitarists
+//         $(".span2 img").map(function(i, image) {
+//             finished_images.push(url.resolve('https://equipboard.com/role/guitarists', $(image).attr('data-original')));
+//         });
+//     });
+
+//
+// let run = () => {
+//     toJson();
+// };
+//
+// setTimeout(() => {
+//     run();
+// }, 2000);
+
+
